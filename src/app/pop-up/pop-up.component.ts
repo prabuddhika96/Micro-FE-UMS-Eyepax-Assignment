7301e67089257eede4b870e0717a2a56573d8562
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-02-28 16:07:19 +0530

    create popup component

---
+++ src/app/pop-up/pop-up.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      pop-up works!
+    </p>
+  `,
+  styleUrl: './pop-up.component.css'
+})
+export class PopUpComponent {
+
+}

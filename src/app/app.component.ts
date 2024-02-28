e86312be743778dcea66e91d3da6ed04f1146546
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-02-28 10:32:41 +0530

    add ant design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NzButtonModule],
+  template: ` <button nz-button nzType="primary">Primary Button</button> `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ums-angular';

bf4e9940301320acdbdc59cae39b3509875586ea
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-02-28 10:47:38 +0530

    install tailwind

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      edit-profile works!
-    </p>
-  `,
-  styleUrl: './edit-profile.component.css'
+  imports: [ReactiveFormsModule],
+  template: ` <section class="">
+    <form [formGroup]="editProfileForm" (submit)="handleSubmit($event)">
+      <label for="first-name">First Name</label>
+      <input type="text" id="first-name" formControlName="firstName" />
+      <label for="last-name">Last Name</label>
+      <input type="text" id="last-name" formControlName="lastName" />
+      <label for="email">Email</label>
+      <input type="text" id="email" formControlName="email" />
+
+      <button class="primary" type="submit">Apply Now</button>
+    </form>
+  </section>`,
+  styleUrl: './edit-profile.component.css',
 })
 export class EditProfileComponent {
+  editProfileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.editProfileForm.value);
+  }
 }

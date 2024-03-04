9b04fc92213e330461b31c03765c7fbd61ef99d5
Madhawa Kavishan <madhawa.k@eyepax.com>
2024-03-04 11:24:08 +0530

    Changed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignupComponent } from './Pages/signup/signup.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'edit-profile',
+        component: EditProfileComponent
+    }
+    // {
+    //     path:'',
+    //     component: DashboardComponent,
+    //     children: [
+    //       {
+    //         path:'dashboard',
+    //         component: DashboardComponent
+    //       }
+    //     ]
+    //   }
+
+];
